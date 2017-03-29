1ad45482edf13566282a70f9e016569124ea976c
XoDeR <xoder.vs@gmail.com>
2017-03-30 00:58:10 +0200

    Amstel Json + Network
    Amstel Json + Network

---
+++ Src/Core/Json/JsonTypes.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+#include "Core/Containers/ContainerTypes.h"
+#include "Core/Strings/FixedString.h"
+
+namespace RioCore
+{
+
+struct JsonValueType
+{
+	enum Enum
+	{
+		NIL,
+		BOOL,
+		NUMBER,
+		STRING,
+		ARRAY,
+		OBJECT
+	};
+};
+
+// Array of pointers to json-encoded data
+using JsonArray = Array<const char*>;
+
+// Map from key to pointers to json-encoded data
+struct JsonObject
+{
+	JsonObject(Allocator& a);
+
+	const char* operator[](const char* key) const;
+	const char* operator[](const FixedString& key) const;
+
+	Map<FixedString, const char*> map;
+};
+
+} // namespace RioCore
+// Copyright (c) 2016 Volodymyr Syvochka
---
+++ Src/Core/Network/NetworkTypes.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2016 Volodymyr Syvochka
+#pragma once
+
+#include "Config.h"
+#if RIO_NETWORK_ENABLED
+namespace RioCore
+{
+
+struct Socket;
+struct IpAddress;
+
+} // namespace RioCore
+#endif // RIO_NETWORK_ENABLED
+// Copyright (c) 2016 Volodymyr Syvochka
